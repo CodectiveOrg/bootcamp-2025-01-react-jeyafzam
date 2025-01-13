7a9cadb2172eaa962153e545d541c9948e00c378
fereshteh <jeyafzamfereshteh87@gmail.com>
2025-01-13 19:22:59 +0330

    [ADD] basic html

--- dream-journal/src/App.tsx
+++ dream-journal/src/App.tsx
@@ -1,13 +1,34 @@
+import MingcuteSearchLine from "./icons/MingcuteSearchLine.tsx";
+import LineMdMoonRisingLoop from "./icons/LineMdMoonRisingLoop.tsx";
+
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(count + 1)}>+ </button>
+      <header>
+        <h1>Dream Journal</h1>
+      </header>
+      <main>
+        <div className="toolbar">
+          <div className="search-box">
+            <input type="text" />
+            <MingcuteSearchLine className="icon" />
+          </div>
+          <select>
+            <option value="">All</option>
+            <option value="">Filter 1</option>
+            <option value="">Filter 2</option>
+          </select>
+          <button className="theme">
+            <LineMdMoonRisingLoop />
+          </button>
+        </div>
+        <ul className="items"></ul>
+      </main>
+      <footer>
+        <button className="add">+</button>
+      </footer>
     </div>
   );
 }
