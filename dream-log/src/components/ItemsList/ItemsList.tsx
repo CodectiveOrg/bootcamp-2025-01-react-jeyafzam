4e3952023ecd29f061122601ae74a71067d3a011
fereshteh <jeyafzamfereshteh87@gmail.com>
2025-01-18 15:15:56 +0330

    [ADD] items list styles

--- dream-log/src/components/ItemsList/ItemsList.tsx
+++ dream-log/src/components/ItemsList/ItemsList.tsx
@@ -1,7 +1,28 @@
+import MingcuteDelete2Line from "../../icons/MingcuteDelete2Line";
+import MingcuteEdit2Line from "../../icons/MingcuteEdit2Line";
+import styles from "./ItemsList.module.css";
 
-const ItemsList = () =>{
-    return(
-        <ul className="items"></ul>
-    )
-}
+const items = ["Note #1", "Note #2", "Note #3"];
+const ItemsList = () => {
+  return (
+    <ul className={styles.items}>
+      {items.map((item) => (
+        <li key={item}>
+          <label>
+            <input type="checkbox" />
+            <div className={styles.title}>{item}</div>
+          </label>
+          <div className={styles.actions}>
+            <button className={styles.edit}>
+              <MingcuteEdit2Line />
+            </button>
+            <button className={styles.delete}>
+              <MingcuteDelete2Line />
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 export default ItemsList;
